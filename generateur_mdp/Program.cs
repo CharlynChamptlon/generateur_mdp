93d3104f66a267d9815a5a09f02ea6a58f7177c8
Charlyn Lonchampt <charlyn.lonchampt@gmail.com>
2023-08-31 11:38:31 +0200

    1er commit

---
+++ generateur_mdp/Program.cs
@@ -0,0 +1,56 @@
+﻿using FormationCS;
+using System;
+
+
+namespace generateur_mdp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const int NB_MDP = 10;
+            int longueur_mdp = outils.DemanderNombrePositifNonNul("Quelle est la longueur du mot de passe ? ");
+            Console.WriteLine(longueur_mdp);
+
+            string minuscules = "abcdefghijklmnopqrstuvwxz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caractereSpec = "?:!*#";
+            string alphabet1 = minuscules;
+            string alphabet2 = alphabet1 + majuscules;
+            string alphabet3 = alphabet2 + chiffres;
+            string alphabet4 = alphabet3 + caractereSpec;
+            Console.WriteLine();
+
+
+            Console.WriteLine("Vous voulez un mot de passe avec :");
+            Console.WriteLine("1 - Uniquement des caractères en minuscule");
+            Console.WriteLine("2 - Des caractères minuscules et majuscules");
+            Console.WriteLine("3 - Des caractères et des chiffres");
+            Console.WriteLine("4 - Caractères, chiffres et caractères spéciaux");
+            int choix = FormationCS.outils.DemanderNombreEntre("Votre choix : ", 1, 4);
+            string alphabet = "";
+
+            if (choix == 1 ) { alphabet = alphabet1; }
+            else if (choix == 2) { alphabet = alphabet2; }
+            else if (choix == 3) { alphabet = alphabet3; }
+            else if (choix == 4) { alphabet = alphabet4; }
+            string mdp = "";
+            Random rand = new Random();
+            for (int j = 0;j < NB_MDP; j++)
+            {
+                mdp = "";
+                for (int i = 0; i < longueur_mdp; i++)
+                {
+                    int caract = rand.Next(0, alphabet.Length);
+                    mdp = mdp + alphabet[caract];
+                }
+                Console.WriteLine(mdp);
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ generateur_mdp/outils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue, "Erreur : le nombre doit être positif et non nul.");
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonnalise = null)
+        {
+
+            int nombre = DemanderNombre(question);
+            if (nombre >= min && nombre <= max) { return nombre; }
+
+            if (messageErreurPersonnalise == null) { 
+                Console.WriteLine($"Erreur : Vous devez entrer un nombre entre {min} et {max}.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPersonnalise);
+                Console.WriteLine();
+            }
+            
+            return DemanderNombreEntre(question, min, max, messageErreurPersonnalise);
+
+        }
+        public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int rep = int.Parse(reponse);
+                    return rep;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: vous devez rentrer un nombre entier!");
+                }
+            }
+        }
+    }
+}
